067110ae1f6374619d925e30d1815273958592ff
zfx1101804091 <1875586562@qq.com>
2020-03-18 08:14:58 +0800

    服务注册与发现（通过负载均衡的方式）

--- nacos-restful-consumer/src/main/java/org/example/controller/ConsumerController.java
+++ nacos-restful-consumer/src/main/java/org/example/controller/ConsumerController.java
@@ -1,9 +1,14 @@
 package org.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 
 /**
  * @description:
@@ -14,14 +19,36 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class ConsumerController {
     
+   /* 
     @Value("${provider.address}")
-    private String provider;
+    private String provider;*/
     
-    @GetMapping(value = "/server")
+   //通过负载均衡发现地址
+    @Autowired
+    LoadBalancerClient loadBalancerClient;
+    
+   /* @GetMapping(value = "/server")
     public String server(){
         String url = "http://"+provider+"/server";
         //调用接口，返回的String类型，所以此处为String.class
         String result = new RestTemplate().getForObject(url, String.class);
+
+        return "Consumer invoke success  "+result;
+    }
+*/
+    //服务id即注册中心的中的服务名 
+    private String serviceId="nacos-restful-provider";
+   
+    
+    @GetMapping(value = "/server1")
+    public String server1(){
+
+        //发现一个地址
+        ServiceInstance choose = loadBalancerClient.choose(serviceId);
+        //获取一个http://ip+端口的地址
+        URI uri = choose.getUri();
+        //调用服务
+        String result = new RestTemplate().getForObject(uri+"/server", String.class);
 
         return "Consumer invoke success  "+result;
     }
